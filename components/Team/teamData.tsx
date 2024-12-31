e0a6625079db4a26ee9b6b6852517987b0a7e9ec
Mushriq Al-Jazrawe <mushriq@mit.edu>
2024-12-31 12:05:32 -0500

    Update team member bios to placeholder text and change SVG stroke color to primary

--- components/Team/teamData.tsx
+++ components/Team/teamData.tsx
@@ -10,7 +10,7 @@ const teamData: TeamMember[] = [
     photo: "/images/team/person1.png",
     github: "",
     linkedin: "https://www.linkedin.com/in/mushriq",
-    paragraph: "Mushriq completed his graduate studies at the University of Toronto in the Developmental & Stem Cell Biology program at the Hospital for Sick Children (Toronto). Moving to the Broad Institute as postdoc, he honed his computational skills to harness existing cancer datasets to formulate hypotheses for rare diseases. Now leading the HTS team, he is excited to apply this experience in building the next generation of datasets. Mushriq is passionate about teaching and he's happiest when there's a whiteboard and a marker nearby.",
+    paragraph: "[Bio and photo coming soon]",
   },
   {
     id: 2,
@@ -18,8 +18,8 @@ const teamData: TeamMember[] = [
     role: "Research Associate",
     photo: "/images/team/person2.png",
     github: "",
-    linkedin: "http://www.google.com",
-    paragraph: "...",
+    linkedin: "",
+    paragraph: "[Bio and photo coming soon]",
   },
   {
     id: 3,
@@ -27,8 +27,8 @@ const teamData: TeamMember[] = [
     role: "Research Associate",
     photo: "/images/team/person3.png",
     github: "",
-    linkedin: "http://www.google.com",
-    paragraph: "...",
+    linkedin: "",
+    paragraph: "[Bio and photo coming soon]",
   },
 ];
 export default teamData;
