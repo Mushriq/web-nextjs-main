1a75ed0a474ac7b8e44db0f3af191ec805754034
Mushriq Al-Jazrawe <mushriq@mit.edu>
2024-12-31 11:33:07 -0500

    Update static URL in barcodeData2 component for production deployment

--- components/Projects/biosensor/barcodeData2.tsx
+++ components/Projects/biosensor/barcodeData2.tsx
@@ -84,7 +84,7 @@ const AxiosGetRequest = () => {
   const TABLE_HEAD = ["Status", "Timepoint", "Compound", "Mechanism", "LogFC Labelled", "LogFC Unlabelled"];
   const [TABLE_ROWS, setTABLE_ROWS] = useState([]);
 
-  const static_url = "http://0.0.0.0:8000" // "https://hts-biosensor-plumber-353269782212.us-central1.run.app";
+  const static_url = "https://hts-biosensor-plumber-353269782212.us-central1.run.app";
 
   // For Printing
   const componentRef = React.useRef(null);
