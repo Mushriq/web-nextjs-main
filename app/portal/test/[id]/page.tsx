6c14000a6a82f08d73eb1472bf8cab141141cea5
Mushriq Al-Jazrawe <mushriq@mit.edu>
2025-01-01 10:48:47 -0500

    Add TestPage component with metadata and section title; implement dynamic sample display

---
+++ app/portal/test/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import SectionTitle from "@/components/Common/SectionTitle";
+
+export const metadata: Metadata = {
+  title: "High Throughput Sciences",
+  description: "High Throughput Sciences (HTS) offers automation and screening capabilities to MIT researchers and external collaborators to generate next-generation datasets for machine learning-driven discoveries.",
+  // other metadata
+};
+
+export default async function TestPage({ params }) {
+
+  const { sample } = await params
+
+  return (
+    <>
+      <section id="contact" className="pt-16 md:pt-20 lg:pt-24">
+        <div className="container">
+          <div className="border-b border-body-color/[.15] pb-4 dark:border-white/[.15] md:pb-6 lg:pb-8">
+            <div className="-mx-4 flex flex-wrap items-center justify-center">
+              <div className="w-full section-animation px-4">
+                <div className="flex flex-wrap items-center justify-start w-full">
+                  <SectionTitle title="Hello" paragraph={`this is id ${sample}`} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
