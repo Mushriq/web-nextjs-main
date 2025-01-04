7419b2e4622ed836501c943c5c64c7f7b48eefe2
Mushriq Al-Jazrawe <mushriq@mit.edu>
2025-01-04 17:04:37 -0500

    Update loading message in DirectReportPage to indicate potential wait time

--- components/Projects/biosensor/DirectReportPage.tsx
+++ components/Projects/biosensor/DirectReportPage.tsx
@@ -277,7 +277,7 @@ export default function DirectReportPage({ selectedBarcodeId }) {
           d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
           stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" className="text-primary">
               </path>
-            </svg><div className="in-line block px-4">Retrieving info..</div>
+            </svg><div className="in-line block px-4">Retrieving info.. This may take up to 5 mins.</div>
             </div>
             </div>
                                       
