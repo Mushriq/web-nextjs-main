d9d181537ddd6c0021a04b639dd1e9c0f9ac9a75
Mushriq Al-Jazrawe <mushriq@mit.edu>
2025-04-21 12:37:30 -0400

    fix: remove trailing slash from static URL in SCInteractPlotter component

--- components/Projects/scinteract/SCInteractPlotter.tsx
+++ components/Projects/scinteract/SCInteractPlotter.tsx
@@ -413,7 +413,7 @@ const AxiosGetRequest = () => {
   const TABLE_HEAD = ["Status", "Timepoint", "Compound", "Mechanism", "LogFC Labelled", "LogFC Unlabelled"];
   const [TABLE_ROWS, setTABLE_ROWS] = useState([]);
 
-  const static_url = "https://scinteract-353269782212.us-central1.run.app/"  // "http://localhost:8000" // 
+  const static_url = "https://scinteract-353269782212.us-central1.run.app"  // "http://localhost:8000" // 
 
   // For Printing
   const componentRef = React.useRef(null);
